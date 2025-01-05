a06a16f2b1a43d3e322c446bdf2b982b075406db
Dario Baumberger <dario.baumberger@gmail.com>
2025-01-05 22:47:41 +0000

    optimized component

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -1,19 +1,23 @@
+<template>
+	<component
+		class="underline hover:decoration-2 hover:text-black transition-all duration-300"
+		:is="tag"
+	>
+		<slot />
+	</component>
+</template>
+
 <script lang="ts">
+/**
+ * A styled link component that can be used as a wrapper around an anchor tag or a router-link.
+ */
 export default {
-  name: 'StyledLink',
-  props: {
-    tag: {
-      type: String,
-      default: 'a',
-    },
-  },
+	name: 'StyledLink',
+	props: {
+		tag: {
+			type: String,
+			default: 'a',
+		},
+	},
 }
 </script>
-<template>
-  <component
-    class="underline hover:decoration-2 hover:text-black transition-all duration-300"
-    :is="tag"
-  >
-    <slot />
-  </component>
-</template>
