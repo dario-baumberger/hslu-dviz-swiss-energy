35288637e2b431f1aebf961f63240a4facbd6291
Dario Baumberger <dario.baumberger@gmail.com>
2025-01-05 22:47:09 +0000

    optimized component

--- src/components/Jumbotron.vue
+++ src/components/Jumbotron.vue
@@ -1,59 +1,63 @@
+<template>
+	<section
+		class="w-full p-4 relative bg-white bg-cover relative snap-start snap-always relative z-10"
+		id="section1"
+	>
+		<div
+			class="absolute top-0 left-0 right-0 bottom-0 bg-cover bg-center"
+			:style="{ backgroundImage: `url(${bgImage})` }"
+		></div>
+		<div class="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-70"></div>
+		<div
+			class="max-w-prose m-auto w-full h-screen p-4 flex items-center justify-center flex-col text-center relative text-white"
+		>
+			<div class="max-w-prose m-auto">
+				<h1 class="text-6xl mb-8 font-sans uppercase font-bold">Swiss Energy</h1>
+				<p class="text-xl md:text-2xl">
+					In 2017, the Swiss public voted in favour of the revised Energy Act. This was the first
+					step in implementing the 2050 Energy Strategy.
+				</p>
+				<p class="text-xl md:text-2xl">How is Switzerland doing?</p>
+
+				<button
+					@click="scrollToSection"
+					class="animate-bounce mt-20 md:mt-40 inline-flex flex-col justify-center items-center hover:text-yellow-400 transition-all duration-300"
+				>
+					Scroll Down
+					<span class="inline-block w-10 h-10">
+						<font-awesome-icon :icon="faBolt" class="text-current text-4xl" />
+					</span>
+				</button>
+			</div>
+		</div>
+	</section>
+</template>
+
 <script lang="ts">
 import bgImage from '@/assets/img/bg-image.webp'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faBolt } from '@fortawesome/free-solid-svg-icons'
 
+/**
+ * A jumbotron section with a background image and a call to action.
+ */
 export default {
-  components: {
-    FontAwesomeIcon,
-  },
-  data() {
-    return {
-      bgImage,
-      faBolt,
-    }
-  },
-  methods: {
-    scrollToSection() {
-      const section = document.getElementById('section2')
-      if (section) {
-        section.scrollIntoView({ behavior: 'smooth' })
-      }
-    },
-  },
+	components: {
+		FontAwesomeIcon,
+	},
+	data() {
+		return {
+			bgImage,
+			faBolt,
+		}
+	},
+	methods: {
+		scrollToSection() {
+			const section = document.getElementById('section2')
+			if (section) {
+				section.scrollIntoView({ behavior: 'smooth' })
+			}
+		},
+	},
 }
 </script>
-<template>
-  <section
-    class="w-full p-4 relative bg-white bg-cover relative snap-start snap-always relative z-10"
-    id="section1"
-  >
-    <div
-      class="absolute top-0 left-0 right-0 bottom-0 bg-cover bg-center"
-      :style="{ backgroundImage: `url(${bgImage})` }"
-    ></div>
-    <div class="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-70"></div>
-    <div
-      class="max-w-prose m-auto w-full h-screen p-4 flex items-center justify-center flex-col text-center relative text-white"
-    >
-      <div class="max-w-prose m-auto">
-        <h1 class="text-6xl mb-8 font-sans uppercase font-bold">Swiss Energy</h1>
-        <p class="text-xl md:text-2xl">
-          In 2017, the Swiss public voted in favour of the revised Energy Act. This was the first
-          step in implementing the 2050 Energy Strategy.
-        </p>
-        <p class="text-xl md:text-2xl">How is Switzerland doing?</p>
-
-        <button
-          @click="scrollToSection"
-          class="animate-bounce mt-20 md:mt-40 inline-flex flex-col justify-center items-center hover:text-yellow-400 transition-all duration-300"
-        >
-          Scroll Down
-          <span class="inline-block w-10 h-10">
-            <font-awesome-icon :icon="faBolt" class="text-current text-4xl" />
-          </span>
-        </button>
-      </div>
-    </div>
-  </section>
-</template>
